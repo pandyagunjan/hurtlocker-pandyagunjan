024510c3be804ee29cad794138d709352d077922
gunjan <pandya.gunjan@gmail.com>
2020-12-14 16:19:47 -0500

    format test case fails

--- src/test/java/JerskSonParserTest.java
+++ src/test/java/JerskSonParserTest.java
@@ -1,8 +1,40 @@
-package java;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.*;
 
 public class JerskSonParserTest {
 
+    @Test
+        public void testFileWriterROT13(){
+            try{
+                BufferedReader readerOriginalFile = new BufferedReader(new FileReader("output.txt"));
+                BufferedReader readerCreatedFile = new BufferedReader(new FileReader("MyPrettyOutput.txt"));
+                String Expected;
+                String Actual;
+                while(( readerOriginalFile.readLine()) != null && (readerCreatedFile.readLine()) != null){
+                    Expected = readerOriginalFile.readLine();
+                    Actual = readerCreatedFile.readLine();
+                    Assert.assertEquals(Expected, Actual);
+                }
+
+                readerOriginalFile.close();
+                readerCreatedFile.close();
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+
+    }
 
 
 
-}
+
+
+
