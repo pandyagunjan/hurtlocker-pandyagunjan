56fbf7ef20090333159160cd190c206caf97b852
gunjan <pandya.gunjan@gmail.com>
2020-12-14 12:05:37 -0500

    Change the return for find methods as sert did not work

--- src/main/java/JerkSonParserList.java
+++ src/main/java/JerkSonParserList.java
@@ -1,3 +1,7 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+import sun.lwawt.macosx.CSystemTray;
+import sun.nio.fs.BsdFileSystemProvider;
+
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,8 +12,13 @@ public class JerkSonParserList {
     String[] parseSingleProduct;
     Product product = new Product();
     ArrayList<Product> products= new ArrayList();
-
+    int countOfErrors=0;
+    String name;
+    String type;
+    private Double price;
+    String expiration;
     public Product pasreTheLine(String output) {
+        ArrayList<Product> listWithoutErrors;
         //Split based on ## and Display it
       splittedLines = SplitBasedOnDelimeter(output);
         for (String s: splittedLines) {
@@ -20,65 +29,86 @@ public class JerkSonParserList {
            //Split each product entry by ;@^%*!
            parseSingleProduct = splittedLines[i].split("[;@^%*!]");
            //Per product , find the group based on field names and populate in the POJO
-           findName();
-           findPrice();
-           findType();
-           findExpirationDate();
-           System.out.println(product.toString());
-           products.add(product);
+           name=findAndSetName();
+           price=findAndSetPrice();
+           type=findAndSetType();
+           expiration=findAndSetExpirationDate();
+         //  System.out.println(product.toString());
+           products.add(new Product(name,price,type,expiration));
        }
+
+//
+//        for (int i = 0; i < products.size(); i++) {
+//            System.out.println(products.get(i).toString());
+//
+//        }
+        listWithoutErrors=removeItemsWithErrors(products);
+       for(int i=0;i< listWithoutErrors.size();i++) {
+             System.out.println(listWithoutErrors.get(i).toString());
+       }
+        System.out.println(countOfErrors);
+
 
         //Browse through the arrayList to display fancy output.
 
         return null;
     }
 
-    private void findExpirationDate() {
+    private String findAndSetExpirationDate() {
         Pattern expiration = Pattern.compile("[eE][xX][pP][iI][rR][aA][tT][iI][oO][nN]:(\\d{1,2}/\\d{1,2}/\\d{4})");
         Matcher matcherExpiration = expiration.matcher(parseSingleProduct[3]);
         if (matcherExpiration.find()) {
             // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
             // Does not work so changed to String instead of Date in POJO
-            product.setExpiration(matcherExpiration.group(1));
-
+            return matcherExpiration.group(1);
         }
+        else return "";
     }
 
-    private void findType() {
+    private String findAndSetType() {
         Pattern type = Pattern.compile("[tT][yY][pP][eE]:(\\w+)");
         Matcher matcherType = type.matcher(parseSingleProduct[2]);
         if (matcherType.find()) {
-            product.setType(matcherType.group(1));
+            return matcherType.group(1);
         }
         else
-            product.setType("");
+            return "";
     }
 
-    private void findPrice() {
+    private Double findAndSetPrice() {
         Pattern price = Pattern.compile("[pP][rR][iI][cC][eE]:(\\d+.\\d{2})");
         Matcher matcherPrice = price.matcher(parseSingleProduct[1]);
         if (matcherPrice.find()) {
-            product.setPrice(Double.parseDouble(matcherPrice.group(1)));
+            return Double.parseDouble(matcherPrice.group(1));
         }
         else
-            product.setPrice(0d);
+            return 0d;
     }
 
 
-    private void findName() {
+    private String findAndSetName() {
         Pattern name = Pattern.compile("[nN][aA][mM][eE]:([a-zA-Z]+)");
         Matcher matcher = name.matcher(parseSingleProduct[0]);
         if (matcher.find()) {
-            product.setName(matcher.group(1));
+            return matcher.group(1);
         }
         else
-            product.setName("");
+           return "";
 
     }
 
     private String[] SplitBasedOnDelimeter(String output) {
         return output.split("#{2}");
     }
-
-
+// Printing output
+    public ArrayList<Product> removeItemsWithErrors(ArrayList<Product> list) {
+        ArrayList<Product> output = list;
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).getName().equals("") || list.get(i).getPrice() == 0d || list.get(i).getType().equals("") || list.get(i).getExpiration().equals("")){
+                output.remove(list.get(i));
+                this.countOfErrors++;
+            }
+        }
+        return output;
+    }
 }
