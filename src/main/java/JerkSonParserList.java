c3782f17358e6fd274db277ce36182f4c62c4688
gunjan <pandya.gunjan@gmail.com>
2020-12-14 11:33:37 -0500

    Finally its fixed and POJO arraylist has all data

--- src/main/java/JerkSonParserList.java
+++ src/main/java/JerkSonParserList.java
@@ -1,13 +1,11 @@
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+//Used https://regexr.com/ and pasted the text file to find the right regular expression with grouping
 public class JerkSonParserList {
     String[] splittedLines;
-    String[] splitBasedonSemicolon;
+    String[] parseSingleProduct;
     Product product = new Product();
     ArrayList<Product> products= new ArrayList();
 
@@ -18,45 +16,65 @@ public class JerkSonParserList {
             System.out.println(s);
 
         }
-        //System.out.println(splitTedLines);
+
         System.out.println(splittedLines.length);
        for(int i=0 ; i < splittedLines.length;i++) {
-           splitBasedonSemicolon = splittedLines[i].split("[;@^%*!]");
-               Pattern name = Pattern.compile("[nN][aA][mM][eE]:([a-zA-Z]+)");
-               Matcher matcher = name.matcher(splitBasedonSemicolon[0]);
-               if (matcher.find()) {
-                   product.setName(matcher.group(1));
-               }
-
-               Pattern price = Pattern.compile("[pP][rR][iI][cC][eE]:(\\d+.\\d{2})");
-               Matcher matcherPrice = price.matcher(splitBasedonSemicolon[1]);
-               if (matcherPrice.find()) {
-                   product.setPrice(Double.parseDouble(matcherPrice.group(1)));
-               }
-               Pattern type = Pattern.compile("[tT][yY][pP][eE]:(\\w+)");
-               Matcher matcherType = type.matcher(splitBasedonSemicolon[2]);
-               if (matcherType.find()) {
-                   product.setType(matcherType.group(1));
-               }
-               Pattern expiration = Pattern.compile("[eE][xX][pP][iI][rR][aA][tT][iI][oO][nN]:(\\d{1,2}/\\d{1,2}/\\d{4})");
-               Matcher matcherExpiration = expiration.matcher(splitBasedonSemicolon[3]);
-               if (matcher.find()) {
-                   // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
-                   product.setExpiration(matcherExpiration.group(1));
-                   //For each array of String set the Product POJO - ArrayList ??
-               }
-               System.out.println(product.toString());
-               products.add(product);
-
-
-
+           //Split each product entry by ;@^%*!
+           parseSingleProduct = splittedLines[i].split("[;@^%*!]");
+           //Per product , find the group based on field names and populate in the POJO
+           findName();
+           findPrice();
+           findType();
+           findExpirationDate();
+           System.out.println(product.toString());
+           products.add(product);
        }
-
-
 
         //Browse through the arrayList to display fancy output.
 
         return null;
+    }
+
+    private void findExpirationDate() {
+        Pattern expiration = Pattern.compile("[eE][xX][pP][iI][rR][aA][tT][iI][oO][nN]:(\\d{1,2}/\\d{1,2}/\\d{4})");
+        Matcher matcherExpiration = expiration.matcher(parseSingleProduct[3]);
+        if (matcherExpiration.find()) {
+            // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+            product.setExpiration(matcherExpiration.group(1));
+            //For each array of String set the Product POJO - ArrayList ??
+        }
+    }
+
+    private void findType() {
+        Pattern type = Pattern.compile("[tT][yY][pP][eE]:(\\w+)");
+        Matcher matcherType = type.matcher(parseSingleProduct[2]);
+        if (matcherType.find()) {
+            product.setType(matcherType.group(1));
+        }
+        else
+            product.setType("");
+    }
+
+    private void findPrice() {
+        Pattern price = Pattern.compile("[pP][rR][iI][cC][eE]:(\\d+.\\d{2})");
+        Matcher matcherPrice = price.matcher(parseSingleProduct[1]);
+        if (matcherPrice.find()) {
+            product.setPrice(Double.parseDouble(matcherPrice.group(1)));
+        }
+        else
+            product.setPrice(0d);
+    }
+
+
+    private void findName() {
+        Pattern name = Pattern.compile("[nN][aA][mM][eE]:([a-zA-Z]+)");
+        Matcher matcher = name.matcher(parseSingleProduct[0]);
+        if (matcher.find()) {
+            product.setName(matcher.group(1));
+        }
+        else
+            product.setName("");
+        //return matcher;
     }
 
     private String[] SplitBasedOnDelimeter(String output) {
